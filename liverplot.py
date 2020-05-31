ebcd3263e77755590bdf9da5ad8c84d3607d28d0
mchiozzi <mchiozzi95@gmail.com>
2020-05-31 00:46:07 -0300

    attribute name changed

--- liverplot.py
+++ liverplot.py
@@ -7,7 +7,7 @@ from matplotlib.animation import FuncAnimation as animation
 mpl.rcParams['toolbar'] = 'None'
 
 
-def begin(file_name, x_axis, file_path='', filter_columns=None, fresh_rate=100, sample=-1, figsize=(15, 6)):
+def begin(file_name, x_axis, file_path='', selected_columns=None, fresh_rate=100, sample=-1, figsize=(15, 6)):
     fig = plt.figure(figsize=figsize)
 
     fig.canvas.set_window_title('Liverplot')
@@ -16,8 +16,9 @@ def begin(file_name, x_axis, file_path='', filter_columns=None, fresh_rate=100, 
         df = pd.read_csv(file_path + file_name)
 
         # If user is going to plot specifc columns, he'll not pass the x_axis inside
-        # filter_columns again. For this convenience, we shall add to the array
-        columns = filter_columns + [x_axis] if filter_columns else df.columns
+        # selected_columns again. For this convenience, we shall add to the array
+        columns = selected_columns + \
+            [x_axis] if selected_columns else df.columns
 
         # Tail do whats suppose to do. By default, Liverplot will plot the whole
         # dataframe, however, it's possible to plot only a sample
@@ -33,6 +34,6 @@ def begin(file_name, x_axis, file_path='', filter_columns=None, fresh_rate=100, 
         plt.legend(loc='upper left')
         plt.tight_layout()
 
-    ani = animation(fig, animate, interval=fresh_rate)
+    a = animation(fig, animate, interval=fresh_rate)
 
     plt.show()
